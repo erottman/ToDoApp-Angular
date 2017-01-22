5dc988d825250a6004a1434471166fae3fc608c1
Erica Rottman <erica7650@gmail.com>
2017-01-22 14:41:58 -0800

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+
+.controller('mainCtrl', function ($scope, dataService) {
+  $scope.helloConsole = dataService.helloConsole;
+
+  $scope.learningNgChange = function() {
+    console.log("An input has cahnged!");
+  };
+
+  $scope.todos = [
+    {"name" : "clean the house"},
+    {"name" : "walk the dog"},
+    {"name" : "mow the lawn"},
+    {"name" : "pay bills"},
+    {"name" : "exercise"},
+    {"name" : "swim"}
+  ]
+
+})
+
+.service('dataService', function () {
+
+    this.helloConsole = function () {
+      console.log('This is the hello console service!');
+    }
+
+});
